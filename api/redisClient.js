31009b1da72e4abd0d2f9dfbd2b6a783605197c2
Surya <srisuryakiranchanda@gmail.com>
2026-02-18 18:12:27 +0530

    Fix Upstash TLS config

--- api/redisClient.js
+++ api/redisClient.js
@@ -1,7 +1,11 @@
 const { createClient } = require('redis');
 
 const client = createClient({
-    url: process.env.REDIS_URL
+    url: process.env.REDIS_URL,
+    socket: {
+        tls: true,
+        rejectUnauthorized: false
+    }
 });
 
 client.on('error', (err) => {
